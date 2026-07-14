38e463a4d010fc53a17f56ace1e59e4b31ac570c
Bijay Khadka <bijaykhadka250@gmail.com>
2026-07-14 14:29:41 -0300

    185. using useEffect for syncing with browser APIs

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,16 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { forwardRef, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 const Modal = forwardRef(function Modal({ children, open }) {
   const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, []);
 
   return createPortal(
     <dialog className="modal" ref={dialog} open={open}>
